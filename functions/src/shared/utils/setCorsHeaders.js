41cacedfd1c9eb3073fc2a987c994cc7b8c48edb
Dimitre Ortt <dimiortt@gmail.comt>
2022-08-11 12:51:06 -0300

    Created utils folder in shared module

---
+++ functions/src/shared/utils/setCorsHeaders.js
@@ -0,0 +1,11 @@
+const setCorsHeaders = (res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+};
+
+module.exports = { setCorsHeaders };
