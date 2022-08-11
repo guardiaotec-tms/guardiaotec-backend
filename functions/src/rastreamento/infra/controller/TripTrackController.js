783accef182f843ac3840d6edb6c4b0f824be57d
Dimitre Ortt <dimiortt@gmail.comt>
2022-08-11 12:53:50 -0300

    Changed infra trackController

--- functions/src/rastreamento/infra/controller/TripTrackController.js
+++ functions/src/rastreamento/infra/controller/TripTrackController.js
@@ -17,6 +17,13 @@ const { saveResponse } = require("../service/saveResponse");
 const {
   MakeGeometriesController,
 } = require("../../../geometriaViagem/infra/controller/MakeGeometriesController");
+const {
+  uploadEventsIntegratedReport,
+} = require("../service/uploadEventsIntegratedReport");
+const { uploadError } = require("../../../shared/infra/service/uploadError");
+const {
+  uploadLatestEventsSummary,
+} = require("../service/uploadLatestEventsSummary");
 
 class TripTrackController {
   constructor() {
@@ -37,6 +44,15 @@ class TripTrackController {
 
   async integrateTrip(trip) {
     const events = await findLatestTripEvents(trip, this.latestEvents);
+    const ps = ["DEI3G78", "EVW5130"];
+    let plate = trip.plannedTrip.plannedVehicle.licensePlate;
+    // console.log(plate);
+    if (ps.includes(plate)) {
+      // const str = this.latestEvents.map((e) => e.placa).join(",");
+      // console.log(str);
+      // console.log(str.includes(plate));
+      // console.log("plate", plate);
+    }
     for (const event of events) {
       await this.integrateEvent(trip, event);
     }
@@ -47,15 +63,22 @@ class TripTrackController {
       const currentTrips = await findCurrentActiveTrips();
       this.latestEvents = await getLatestEvents();
       // this.latestEvents = dummyEvents();
-      // return;
       const promises = currentTrips.map((trip) => this.integrateTrip(trip));
       await Promise.all(promises).then(() => {
         console.log(this.eventsIntegrated.map((e) => e.placa).join(","));
         console.log("integrated " + this.eventsIntegrated.length + " events");
       });
 
-      this.makeGeometriesController.main(this.latestEvents);
-    } catch (error) {}
+      await this.makeGeometriesController.main(this.latestEvents);
+      uploadEventsIntegratedReport(this.eventsIntegrated);
+      uploadLatestEventsSummary(this.latestEvents);
+    } catch (error) {
+      console.log("Error in TripTrackController main: ", error.message);
+      uploadError({
+        where: "track_main",
+        msg: "Error in TripTrackController main: " + error.message,
+      });
+    }
     // for (const trip of currentTrips) {
     // }
     /*
@@ -71,3 +94,15 @@ class TripTrackController {
 module.exports = {
   TripTrackController,
 };
+
+/**
+ * 
+ 
+16765497  RNV1C15
+0 12
+16765492  RNU1B81
+0 1
+16921366  JBA8A85
+0 15
+16765504  JAZ5E74
+ */
